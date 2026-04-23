caef962bb19a934962ae5ea904cef7af198c343f
jg211 <jbbarrosantos@gmail.com>
2026-04-23 20:08:05 -0300

    Create exec6.c

---
+++ lista3/exec6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int codigoValido, jaUsouCupom;
+    float valorCompra;
+
+    printf("Codigo valido? (1/0): ");
+    scanf("%d", &codigoValido);
+
+    printf("Digite o valor da compra: ");
+    scanf("%f", &valorCompra);
+
+    printf("Ja usou cupom? (1/0): ");
+    scanf("%d", &jaUsouCupom);
+if(codigoValido == 0){
+printf("CUPOM INVALIDO");}
+else if (codigoValido == 1 && jaUsouCupom == 0 && valorCompra >= 100){
+    printf("DESCONTO APLICADO");
+}else{
+    printf("CUPOM INVALIDO PARA USO");
+}
+
+
+    return 0;
+}
