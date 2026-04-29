06faf927041b032fa7fff3ab9f494e43dad6447c
jg211 <jbbarrosantos@gmail.com>
2026-04-29 20:14:07 -0300

    Create exec3.c

---
+++ lista4/exec3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, numero, soma = 0;
+    for(i = 1; i <+5 ; i++){
+        scanf("%d", &numero);
+        if(numero > 0){
+        soma = soma + numero;
+        }
+    }
+printf("Soma: %d\n", Soma);
+return 0;
+}
