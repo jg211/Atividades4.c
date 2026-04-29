2acb2e7feeef9bca0ecd9980df3665def67083e8
jg211 <jbbarrosantos@gmail.com>
2026-04-29 20:14:27 -0300

    Create exec4.c

---
+++ lista4/exec4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i, maiorQ50 = 0;
+    for(i = 0; i <5 ; i++){
+        scanf("%d", &numero);
+      #include <stdio.h>
+
+int main() {
+    int i, numero, soma = 0;
+    for(i = 1; i <+5 ; i++){
+        scanf("%d", &numero);
+        if(numero > 0){
+        soma = soma + numero;
+        }
+    }
+printf("Soma: %d\n", Soma);
+return 0;
+}  if(numero > 50){
+        maiorQ50++;
+        }
+    }
+printf("Total de números maiores que 10: %d\n", maiorQ50);
+return 0;
+}
