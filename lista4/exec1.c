d12d7170771423f50f19a9e39db5bbaf44dfd480
jg211 <jbbarrosantos@gmail.com>
2026-05-01 11:40:53 -0300

    Update exec1.c

--- lista4/exec1.c
+++ lista4/exec1.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
 int main() {
-int i;
-for (i = 1; i <= 10; i++) {
-    printf("%d\n", i);
-}
-    return 0;
-}#include <stdio.h>
-
-int main() {
-int i;
-for (i = 1; i <= 10; i++) {
-    printf("%d\n", i);
-}
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
     return 0;
 }
