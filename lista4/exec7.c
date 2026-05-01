77dc8b0b86766f84d47c0116c1f29fd5e3c53c83
jg211 <jbbarrosantos@gmail.com>
2026-05-01 11:52:20 -0300

    Update exec7.c

--- lista4/exec7.c
+++ lista4/exec7.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int numero, soma = 0;
+
+    while (soma <= 100) {
+        printf("Informe um número:\n");
+        scanf("%d", &numero);
+        soma += numero;
+    }
+
+    printf("Soma final: %d\n", soma);
+
+    return 0;
+}
