9d5c89943aa8b0f19567bce84ac4fcb5be015c56
jg211 <jbbarrosantos@gmail.com>
2026-05-01 11:48:40 -0300

    Update exec6.c

--- lista4/exec6.c
+++ lista4/exec6.c
@@ -2,14 +2,14 @@
 
 int main() {
     int senha;
-    printf("Informe a sua senha\n");
+
     scanf("%d", &senha);
+
     while (senha != 1234) {
-        printf("Informe a senha novamente:\n");
         scanf("%d", &senha);
-    } if(senha == 1234){
-          printf("Login completo");  
     }
-   
-return 0;
+
+    printf("ACESSO LIBERADO\n");
+
+    return 0;
 }
