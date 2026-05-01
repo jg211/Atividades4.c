f5b2802bce384e1b0731492e8b58299daaca8b71
jg211 <jbbarrosantos@gmail.com>
2026-05-01 11:48:14 -0300

    Update exec5.c

--- lista4/exec5.c
+++ lista4/exec5.c
@@ -2,16 +2,21 @@
 
 int main() {
     int numero, total = 0, positivos = 0;
+
     scanf("%d", &numero);
+
     while (numero != 0) {
         total++;
-        if(numero > 0) {
+
+        if (numero > 0) {
             positivos++;
         }
-        printf("Informe o número:\n");
+
         scanf("%d", &numero);
     }
+
     printf("Total: %d\n", total);
     printf("Positivos: %d\n", positivos);
-return 0;
+
+    return 0;
 }
