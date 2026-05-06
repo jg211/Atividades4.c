a1f7791cd1dd53598eccb7dac3f38f4316fdcd07
jg211 <jbbarrosantos@gmail.com>
2026-05-06 19:22:58 -0300

    Update exec1.c

--- lista5/exec1.c
+++ lista5/exec1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+ int i, codigo;
+ int validos = 0, invalidos = 0;
+ for(i = 1; i = 10; i++){
+     scanf("%d", &codigo);
+     if(codigo >= 1000 & codigo <= 9999){
+     printf("Codigo válido");
+      }
+     else{
+         printf("Codigo inválido");
+ }
+ }
+    return 0;
+}
