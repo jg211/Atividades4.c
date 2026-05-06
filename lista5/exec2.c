83aba2f838a8c30ecb0e865575af837f04f4a832
jg211 <jbbarrosantos@gmail.com>
2026-05-06 19:58:24 -0300

    Update exec2.c

--- lista5/exec2.c
+++ lista5/exec2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+ float meta, valorMes, total = 0;
+ int mesesValidos = 0;
+ printf("Digite a meta de economia: ");
+ scanf("%f", &meta);
+ while(total < meta);{
+ scanf("%d", &valorMes);
+ if(valorMes > 0){
+     total += mesesValidos;
+     mesesValidos++;
+ }else{
+     printf("VALOR DESCONSIDERADO");
+}
+ }
+ printf("Total economizado: %.2f", total);
+  printf("Meses validos: %d", mesesValidos);
+
+    return 0;
+}
