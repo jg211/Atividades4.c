7c6074934eef8b9fa3696da239fb9f9a151b3cb3
jg211 <jbbarrosantos@gmail.com>
2026-05-13 20:20:44 -0300

    Create exec6.c

---
+++ lista6/exec6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+int numeros[10];
+int pos, valor = 0, encontrou = 0;
+for(pos= 0; pos < 10; pos++){
+    printf("Digite o núnero da posição %d: ",pos);
+    scanf("%d", &numeros[pos]);
+}
+printf("Digite o valor a ser buscado: ");
+scanf("%d", &valor);
+for(pos = 0; pos < 10; pos++){
+    if (numeros[pos] == valor){
+        encontrou = 1;
+    }
+}
+if(encontrou){
+    printf("Valor encontrado!");
+}else{
+    printf("Valor não encontrado");
+}
+    return 0;
+}
