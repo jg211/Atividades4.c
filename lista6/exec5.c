6162822e7a65911b25dc15885b6a5018bee66c37
jg211 <jbbarrosantos@gmail.com>
2026-05-13 20:15:40 -0300

    Update exec5.c

--- lista6/exec5.c
+++ lista6/exec5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+float numeros[6];
+int pos; 
+float soma = 0, media;
+ for(pos = 0; pos < 6; pos++){
+     printf("Digite o número da posição %i: ", pos);
+     scanf("%i", &numeros[pos]);
+ }
+ for (pos = 0; pos < 6 ; pos++){
+  soma+= numeros[pos];
+ }
+ media = soma / 6;
+printf("Media = %.2f\n", media);
+     
+ 
+    return 0;
+}
 
