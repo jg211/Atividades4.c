6ff4bc49235c490899c0eff8dbce7b141748e219
jg211 <jbbarrosantos@gmail.com>
2026-05-13 17:15:01 -0300

    Update exec4.c

--- lista5/exec4.c
+++ lista5/exec4.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+int i , status;
+int concluidas = 0, pendentes = 0;
+for(i = 1; i <=8; i++){
+    scanf("%i", &status);
+if(status == 1){
+    concluidas++;
+}else{
+ pendentes++;
+}
+}
+printf("Concluidas:%i", concluidas);
+printf("Pendentes:%i", pendentes);
+
+
+
+    return 0;
+}
