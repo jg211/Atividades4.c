a1b25cb33b3c177a820a4c8f3ad9872653017565
jg211 <jbbarrosantos@gmail.com>
2026-05-13 19:41:43 -0300

    Update exec3.c

--- lista6/exec3.c
+++ lista6/exec3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+int numeros[10];
+int pos, maior;
+ for(pos = 0; pos < 8; pos++){
+     printf("Digite o número da posição %i: ", pos);
+     scanf("%i", &numeros[pos]);
+  }
+ for(pos = 0; pos < 8; pos++){
+     if(numeros[pos] == 1){
+      maior =  numeros[pos];
+ }else{
+     if(numeros[pos] > maior){
+     maior = numeros[pos];
+ }
+ }
+  }
+printf("Maior valor = %d\n", maior);
+  return 0;
+}
