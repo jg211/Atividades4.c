c29f4626faf61bc41f0a54175c3350e06447ac06
jg211 <jbbarrosantos@gmail.com>
2026-05-13 19:23:01 -0300

    Update exec2.c

--- lista6/exec2.c
+++ lista6/exec2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+int numeros[8];
+int i, soma = 0;
+ for(i = 0; i < 8; i++){
+     printf("Digite o número da posição %i: ", i);
+     scanf("%i", &numeros[i]);
+     soma += numeros[i];
+ }
+printf("Soma = %i\n", soma);
+     
+ 
+    return 0;
+}
