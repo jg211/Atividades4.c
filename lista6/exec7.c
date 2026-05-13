83812ae31715b3063e9e63525b7b89f7d787209b
jg211 <jbbarrosantos@gmail.com>
2026-05-13 20:44:45 -0300

    Update exec7.c

--- lista6/exec7.c
+++ lista6/exec7.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-int numeros[10];
+int numeros[8];
 int pos;
- for(pos = 0; pos < 10; pos++){
+ for(pos = 0; pos < 8; pos++){
 printf("Digite o número da posição %i: ",pos);
 scanf("%i", &numeros[pos]);
 }
 printf("Valores digitados:\n");
-for(pos = 9; pos >= 0; pos--){
+for(pos = 8; pos >= 0; pos--){
     printf("numeros[%d] = %d\n",pos, numeros[pos]);
 }
     return 0;
