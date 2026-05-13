f435890edcc0461b10d6fc8d0b5d5d7335ce1f83
jg211 <jbbarrosantos@gmail.com>
2026-05-13 19:49:31 -0300

    Update exec4.c

--- lista6/exec4.c
+++ lista6/exec4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+int numeros[12];
+int pos, pares = 0;
+ for(pos = 0; pos < 12; pos++){
+     printf("Digite o número da posição %i: ", pos);
+     scanf("%i", &numeros[pos]);
+  }
+ for(pos = 0; pos < 12; pos++){
+     if(numeros[pos] %2 == 0){
+     pares++;
+ }
+ }
+  
+printf("Quantidade de pares = %d\n", pares);
+  return 0;
+}
