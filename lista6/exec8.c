707c0a22d6fb8fde1d0ae13c11d9a916c29e628d
jg211 <jbbarrosantos@gmail.com>
2026-05-13 20:44:05 -0300

    Update exec8.c

--- lista6/exec8.c
+++ lista6/exec8.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+int numeros[10];
+int pos;
+ for(pos = 0; pos < 10; pos++){
+     printf("Digite o número da posição %i: ", pos);
+     scanf("%i", &numeros[pos]);
+       if(numeros[pos] < 0){
+ numeros[pos] = 0;
+  }
+ }
+ for(pos = 0; pos < 10; pos++){
+ 
+ printf("numeros[%d] = %d\n",pos, numeros[pos]);
+ }
+ 
+  return 0;
+}
